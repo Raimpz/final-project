0f65b0ac2b7b0df639e5cceb05c6d44927e2b868
DESKTOP-H7TUMSI\raimo <raimond.kurm@gmail.com>
2022-05-14 11:01:18 +0300

    add header

--- shopping-list/src/App.vue
+++ shopping-list/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <main>
+    <the-header></the-header>
     <div class="container">
       <shopping-list></shopping-list>
     </div>
@@ -8,10 +9,12 @@
 
 <script>
 import ShoppingList from "./components/ShoppingList.vue";
+import TheHeader from "./components/layouts/TheHeader.vue";
 
 export default {
   components: {
     ShoppingList,
+    TheHeader,
   },
 };
 </script>
---
+++ shopping-list/src/components/layouts/TheHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav>
+    <h1>HEADER</h1>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  width: 100%;
+  height: 75px;
+  background: beige;
+}
+</style>
