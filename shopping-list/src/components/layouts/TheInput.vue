b141c0078ec36f9204e9bc8039356375eb56d28e
Siim Siirak <siimsiirak@hotmail.com>
2022-05-15 12:24:07 +0300

    input styling

--- shopping-list/src/components/layouts/TheInput.vue
+++ shopping-list/src/components/layouts/TheInput.vue
@@ -48,4 +48,44 @@ form div {
 form div h3 {
   color: red;
 }
+
+</style>
+
+<style scoped>
+
+
+form {
+  /* background-color: red;  */
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 10px;
+  padding: 0 10px;
+  height: 30px;
+  
+}
+input {
+  width: 100%;
+  height: 100%;
+  padding: 0 10px;
+  font-size: 15px;
+}
+button {
+  height: 25px;
+  border: 2px solid rgb(68, 38, 219);
+  font-weight: 75px;
+  background-color: rgb(201, 201, 244);
+  padding: 0px 0px;
+  border-radius: 5px;
+  color: black;
+  padding: 3px;
+}
+font div {
+  position: absolute;
+  top: -150%;
+  width: 100%;
+  text-align: center;
+}
+
 </style>
